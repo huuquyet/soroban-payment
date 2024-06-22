b7dade8b000658989b11875a24115effd8649244
Quyet <huuquyetng@gmail.com>
2024-06-22 19:43:05 +0700

    feat: add analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+import { inject } from '@vercel/analytics'
 import ReactDOM from 'react-dom/client'
-
 import { SendPayment } from './components/send-payment'
 
 import '@stellar/design-system/build/styles.min.css'
 import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
+inject()
 root.render(<SendPayment />)
